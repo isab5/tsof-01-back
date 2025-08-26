cd738b3cc2348125c489d884aeb02b1c560ed396
Isa <isabella.b.rosa6@aluno.senai.br>
2025-08-26 14:15:50 -0300

    Create somar test

---
+++ _tests_/calculadora.test.js
@@ -0,0 +1,16 @@
+import { somar } from "../src/calculadora.js";
+
+describe("Calculadora", () => {
+    test("Deve somar dois números corretamente", () => {
+        //AAA
+        //Arrange (Preparar)
+        const a = 5;
+        const b = 7;
+        
+        // Act (Executar)
+        const resultado = somar(a, b);
+
+        // Assert (Verificar)
+        expect(resultado).toBe(12);
+    });
+});
